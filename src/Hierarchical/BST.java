d984d4804a9928431a0038996601c61c84353c95
mainaGioia <alessandra.manna89@gmail.com>
2018-06-07 20:25:51 +0200

    BST

---
+++ src/Hierarchical/BST.java
@@ -0,0 +1,53 @@
+package Hierarchical;
+
+public class BST {
+	
+	public Node root;
+	
+	public void add(int val) {
+		root = insert(val, root);
+	}
+	
+	public Node insert(int val, Node root) {
+		if(root == null) {
+			return new Node<Integer>(val);
+		}
+		
+		else if(val < (int)root.value) 
+			root.left = insert(val, root.left);
+		
+		else
+			root.right = insert(val, root.right);
+		
+		return root;
+
+	}
+	
+	public String toString() {
+		return toString(root);
+	}
+	
+	
+	public String toString(Node n) {
+		String s = "";
+		if(n == null)
+			return " ";
+		s += toString(n.left);
+		s += n.value;
+		s += toString(n.right);
+		return s;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		BST bst = new BST();
+		bst.add(3);
+		bst.add(4);
+		bst.add(5);
+		bst.add(6);
+		System.out.println("bst (3) : "+bst.toString());
+	}
+	
+
+}
