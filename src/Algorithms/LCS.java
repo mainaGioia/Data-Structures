967f9c8518c63e1b3d4be630f5b4e73c4891979c
mainaGioia <alessandra.manna89@gmail.com>
2018-07-13 15:02:39 +0200

    Omg

--- src/Algorithms/LCS.java
+++ src/Algorithms/LCS.java
@@ -2,9 +2,10 @@ package Algorithms;
 
 public class LCS {
 		
-	public int longestCommonSubsequence(char[] A, char[] B, int m, int n) {
+	public String longestCommonSubsequence(char[] A, char[] B, int m, int n) {
 		//i add a column and a row to allow the case L[1][1] = L[i-1]... j-1 otherwise it goes out of b
 		int L[][] = new int[m+1][n+1];
+		StringBuffer LCS = new StringBuffer();
 		
 		for (int i = 0; i < m+1; i++)
 			for (int j = 0; j < n+1; j++) 
@@ -15,7 +16,21 @@ public class LCS {
 					 L[i][j] = A[i-1] == B[j-1] ? L[i-1][j-1]+1 : max(L[i][j-1], L[i-1][j]);
 			}
 		
-		return L[m][n];		
+		int i = m;
+		int j = n;
+		while (i > 0 && j > 0) {
+			if(A[i-1] == B[j-1]) {//cause i added a column and a row for the 0s
+				LCS.append(A[i-1]);
+				i--;
+				j--;
+			} //we go to the entry with the largest value
+			else if (L[i][j-1] > L[i-1][j])
+				j--;
+			else
+				i--;			
+		}
+		
+		return LCS.toString();	
 	}
 	
 	
@@ -35,8 +50,19 @@ public class LCS {
 	    int m = X.length;
 	    int n = Y.length;
 	 
-	    System.out.println("Length of LCS is" + " " +
+	    System.out.println("LCS is" + " " +
 	                                  lcs.longestCommonSubsequence( X, Y, m, n ) );
+	    
+	    String s3 = "ABCBDAB";
+	    String s4 = "BDCABA";
+	    char[] X1=s3.toCharArray();
+	    char[] Y1=s4.toCharArray();
+	    int m1 = X1.length;
+	    int n1 = Y1.length;
+	 
+	    System.out.println("LCS is" + " " +
+	                                  lcs.longestCommonSubsequence( X1, Y1, m1, n1 ) );
+	    
 	  }
 
 }
